c0901bd7955a4f81ea8b08f766fe1aa4f568f096
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-07-01 23:54:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ExpenceItem from './Componant/ExpenceItem';
+
+
+function App() {
+  return (
+    <div className="App">
+    <ExpenceItem/>
+    </div>
+  );
+}
+
+export default App;
