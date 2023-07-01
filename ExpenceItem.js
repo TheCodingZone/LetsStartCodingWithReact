980f81e58c89a9307d20519eb0fb219f1e095cdf
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-07-01 23:54:37 +0530

    Add files via upload

---
+++ ExpenceItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ExpenceItem() {
+  return (
+    <div>
+      <h2>Expence Item</h2>
+      <h3>Food RS 10</h3>
+      <h3>Petrol Rs 100</h3>
+      <h3>Movies Rs 200</h3>
+      
+
+    </div>
+  )
+}
